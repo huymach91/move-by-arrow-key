751396cf365890f238edc3cb4e0445142197ab5e
huymach91 <huy.mach.webdev@gmail.com>
2022-06-10 17:10:31 +0700

    1 file modified

--- line.js
+++ line.js
@@ -5,6 +5,8 @@ export class Line {
   }
 
   createLine(p5) {
+    p5.strokeWeight(3);
+    p5.stroke('#2b78e4');
     this.element = p5.line(this.start.x, this.start.y, this.end.x, this.end.y);
   }
 }
