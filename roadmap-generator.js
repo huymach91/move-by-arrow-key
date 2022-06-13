5849982aabf196bb1d1dbdceee4394fc8f022560
huymach91 <huy.mach.webdev@gmail.com>
2022-06-13 07:46:24 +0700

    1 file modified

--- roadmap-generator.js
+++ roadmap-generator.js
@@ -55,12 +55,13 @@ export class RoadmapGenerator {
           const isLeft = currentChildIndex % 2 !== 0 ? true : false;
           currentChildData.isLeft = isLeft;
           // currentChildData.x = isLeft ? currentRootData.x - rootElementWidth - 50 : currentRootData.x + rootElementWidth + 50;
-          const childLength = currentChildData.name.length * 0.125;
-          const leftX =
-            currentRootData.x - rootElementWidth - childLength - this.intervalX;
-          const rightX =
-            currentRootData.x + rootElementWidth + childLength + this.intervalX;
+          const childLength = currentChildData.name.length * 6;
+          let leftX = currentRootData.x - rootElementWidth - this.intervalX;
+          const rightX = currentRootData.x + rootElementWidth + this.intervalX;
           // create leafs (calc x-axis)
+          if (isLeft) {
+            leftX -= childLength;
+          }
           if (isFirstLevel) {
             currentChildData.x = isLeft ? leftX : rightX;
           } else {
