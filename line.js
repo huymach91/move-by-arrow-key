67e730b6f411395ec0320860f6614f3f675c0839
huymach91 <huy.mach.webdev@gmail.com>
2022-06-13 17:28:18 +0700

    1 file modified

--- line.js
+++ line.js
@@ -2,11 +2,32 @@ export class Line {
   constructor(start, end, optional) {
     this.start = start;
     this.end = end;
+    this.optional = optional || { isBezierCurve: false };
   }
 
   createLine(p5) {
     p5.strokeWeight(3);
     p5.stroke('#2b78e4');
-    this.element = p5.line(this.start.x, this.start.y, this.end.x, this.end.y);
+    if (this.optional.isBezierCurve) {
+      p5.noFill();
+      p5.beginShape();
+      p5.vertex(this.start.x, this.start.y); // first point
+      p5.bezierVertex(
+        this.start.x,
+        this.start.y,
+        this.start.x + 60,
+        this.start.y + 60,
+        this.end.x,
+        this.end.y
+      );
+      p5.endShape();
+    } else {
+      this.element = p5.line(
+        this.start.x,
+        this.start.y,
+        this.end.x,
+        this.end.y
+      );
+    }
   }
 }
