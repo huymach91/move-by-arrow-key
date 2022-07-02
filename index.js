206bb699789b6db5a3f74db911b8fb795e4de4c2
huymach91 <huy.mach.webdev@gmail.com>
2022-07-02 07:22:33 +0700

    2 files modified

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import { RoadmapStartup } from './src/index';
 import rawData from './src/sample-data.json';
 
+const container = document.getElementById('container');
+const bounding = container.getBoundingClientRect();
+const width = bounding.width;
+const height = bounding.height;
 const rm = new RoadmapStartup(rawData.samples2, {
   title: 'Front-end',
+  width: width,
+  height: height,
+  container: container,
+  markDoneFunc: () => {},
 });
-
 rm.startup();
